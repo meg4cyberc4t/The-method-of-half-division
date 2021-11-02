8b998a926c58623813ff93b74c59c07aebe01995
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-11-02 10:16:01 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+
+def solve(function, variable):
+    print(function.replace("x", f"({str(variable)})"))
+    answer = float(eval(function.replace("x", f"({str(variable)})")))
+    print(answer)
+    return answer
+
+def main():
+    os.system('clear')
+    formula = input('f(x) = ')
+    min = float(input("min = "))
+    max = float(input("max = "))
+
+    if (solve(formula, min) * solve(formula, max) > 0):
+        print('Корней нет или больше одного')
+        exit(0)
+
+    vos = False if solve(formula, min) > solve(formula, (min + max) / 2) else True
+    print(f"{vos}")
+
+    while True:
+        middleX = (min + max) / 2
+        middleY = solve(formula, middleX)
+        print(f"{middleX=}, {middleY=}")
+        if middleY == 0:
+            break
+        if not vos and middleY < 0:
+            min, max = min, middleX
+        elif vos and middleY > 0:
+            min, max = min, middleX
+        else: 
+            min, max = middleX, max 
+        
+
+if __name__ == "__main__":
+    main()
